93b0f9cc98ee2b5ae424dda6f69d4b489969a388
Miguel <miguellmarqques17@gmail.com>
2025-11-05 10:47:10 +0000

    add classe

---
+++ ProjetoFinal/ProjetoFinancas/Classes/Administrador.cs
@@ -0,0 +1,12 @@
+public class Administrador  {
+
+    public void GerirUtilizadores() {
+
+        Console.WriteLine("A gerir utilizadores...");
+    }
+
+    public void GerarRelatorios() {
+
+        Console.WriteLine("A gerar relatórios...");
+    }
+}
